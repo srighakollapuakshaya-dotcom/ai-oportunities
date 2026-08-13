d052fc5a74077eb2030d980d9ead05123341257c
srighakollapuakshaya-dotcom <srighakollapuakshaya@gmail.com>
2026-08-13 12:15:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ def login_page():
                     st.session_state.page = "dashboard"
 
                     st.switch_page(
-                        "pages/dashboard.py"
+                        "dashboard.py"
                     )
 
                 else:
@@ -411,7 +411,7 @@ def google_accounts():
         st.session_state.page = "dashboard"
 
         st.switch_page(
-            "pages/dashboard.py"
+            "dashboard.py"
         )
 
 
@@ -449,7 +449,7 @@ def google_accounts():
         st.session_state.page = "dashboard"
 
         st.switch_page(
-            "pages/dashboard.py"
+            "dashboard.py"
         )
 
 
@@ -607,7 +607,7 @@ def signup_page():
 if st.session_state.logged_in:
 
     st.switch_page(
-        "pages/dashboard.py"
+        "dashboard.py"
     )
 
 
